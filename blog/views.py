2012442ed8f9862f9621f7030c2ec77223011724
Niall O'Kane <okanenm@gmail.com>
2015-09-15 16:54:50 -0700

    Create view for a GET/POST to /register

---
+++ blog/views.py
@@ -0,0 +1,23 @@
+from models import User, get_todays_recent_posts
+from flask import Flask, request, session, redirect, url_for, render_template, flash
+
+app = Flask(__name__)
+
+@app.route('/register', methods=['GET','POST'])
+def register():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+
+        if len(username) < 1:
+            flash('Your username must be at least one character.')
+        elif len(password) < 5:
+            flash('Your password must be at least 5 characters.')
+        elif not User(username).register(password):
+            flash('A user with that username already exists.')
+        else:
+            session['username'] = username
+            flash('Logged in.')
+            return redirect(url_for('index'))
+
+    return render_template('register.html')
