ae0aaf09d37d235bcc9206499d7c2c00d4381991
Niall O'Kane <okanenm@gmail.com>
2015-09-15 17:25:03 -0700

    Implement timestamp() and date()

--- blog/models.py
+++ blog/models.py
@@ -46,3 +46,12 @@ class User:
             return bcrypt.verify(password, user['password'])
         else:
             return False
+
+def timestamp():
+    epoch = datetime.utcfromtimestamp(0)
+    now = datetime.now()
+    delta = now - epoch
+    return delta.total_seconds()
+
+def date():
+    return datetime.now().strftime('%Y-%m-%d')
